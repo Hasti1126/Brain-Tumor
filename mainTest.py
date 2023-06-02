75d4472e4e2e502a37c75cd6568c5e44fd2a41c1
Hasti Chopada <hastichopada11@gmail.com>
2023-06-02 22:54:35 +0530

    Intial Commit

---
+++ mainTest.py
@@ -0,0 +1,26 @@
+import cv2
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+model=load_model('BrainTumor10Epochs.h5')
+
+image=cv2.imread('uploads\pred1.jpg')
+
+img=Image.fromarray(image)
+
+img=img.resize((64,64))
+
+img=np.array(img)
+
+input_img=np.expand_dims(img, axis=0)
+
+result=model.predict(input_img)
+
+print(result)
+intr=np.max(model.predict(input_img), axis=0)
+print(intr)
+
+
+
+
